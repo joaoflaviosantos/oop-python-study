8f8a2f5caa3687d5a85208ce913e0341d5601b36
joaoflaviosantos <joaoflavio.s@hotmail.com>
2021-07-24 00:49:25 -0300

    other commit

---
+++ files/03_getters_setters_states.py
@@ -0,0 +1,29 @@
+############## ClASSES CODES #################
+
+class Alarm:
+
+    def __init__(self, state: bool) -> None:
+        self.__state = state # private atribute
+
+    def get_state(self) -> bool:
+        return self.__state
+
+    def set_state(self, value: bool) -> None:
+        if isinstance(value, bool):
+            self.__state = value
+        else:
+            print("Incorrect value! '{}' isn't boolean value." .format(value))
+
+
+
+################# TESTS ####################
+
+print("\n############## GETTER AND SETTERS TEST ################\n")
+
+al = Alarm(False)
+
+print(al.get_state())
+al.set_state(True)
+print(al.get_state())
+al.set_state(1)
+print(al.get_state())
--- readme.md
+++ readme.md
@@ -5,5 +5,6 @@
 https://www.youtube.com/channel/UCBYJKJXaigXVTVsr2UmtWyg
 
 #### 01 - Class and context (https://www.youtube.com/watch?v=CtSX3dWqWBQ)
-
 #### 02 - Private Encapsulation (https://www.youtube.com/watch?v=RJWucpLGBng)
+
+#### 03 - Getters, Setters and States (https://www.youtube.com/watch?v=rHSQ8oam8bQ)
